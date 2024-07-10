70c7b6cdb474420843ad2ff693f52bf6c7148cf8
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-07-10 17:21:36 -0400

    fix modal (create new component for it) for past player data

--- src/components-pages/Players.js
+++ src/components-pages/Players.js
@@ -6,6 +6,11 @@ import { getLeagueName } from "./AuthContent";
 const Players = () => {
 
     const [availablePlayers, setAvailablePlayers] = useState([]);
+    /*const [playerName, setPlayerName] = useState("");
+
+    const trimPlayerName = ( playername ) => {
+        return playername.substring(0, playername.indexOf(" ("));
+    }*/
 
     const getAvailablePlayers = () => {
         const queryString = `/get-all-players?leagueName=${getLeagueName()}`;
@@ -41,16 +46,41 @@ const Players = () => {
                 <table className="table">
                     <thead>
                         <tr>
+                            <th scope="col">View Past Data</th>
                             <th scope="col">Full Name</th>
                             <th scope="col">Position</th>
                         </tr>
                     </thead>
                 <tbody>
                     {availablePlayers.map((player) => (
-                            <tr key={player.id}>
-                                <th scope="row">{player.fullName}</th>
-                                <td>{player.position}</td>
-                            </tr>
+                        <tr key={player.id}>
+                            <th>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    View Player History
+                                </button>
+
+                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+
+                                            <div class="modal-body">
+                                                PLACEHOLDER
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </th>
+                            <th scope="row">{player.fullName}</th>
+                            <td>{player.position}</td>
+                        </tr>
                     ))}
                 </tbody>
                 </table>
