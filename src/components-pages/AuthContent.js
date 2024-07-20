695ff65b5671eb1703a967b18a8e8625252701ea
Brandon Fox <brandon@Brandons-MacBook-Pro-5.local>
2024-07-19 21:59:33 -0400

    .

--- src/components-pages/AuthContent.js
+++ src/components-pages/AuthContent.js
@@ -4,6 +4,7 @@
 import React , { useState, useEffect } from "react";
 import { getAuthToken, getUsername, request } from "../axios_helper";
 import { Link, Route, Routes } from "react-router-dom";
+import { Button, Popover, OverlayTrigger } from 'react-bootstrap';
 import Navbar from "./AuthNavbar";
 import { setAuthToken } from "../axios_helper";
 import CreateLeague from "./CreateLeague";
@@ -26,6 +27,18 @@ const AuthContent = () => {
             setAuthToken(null);
         }).catch(error => console.error('Logout error:', error));
     };
+
+    const getTeamName = () => {
+        const querystring = "/get-team?leagueName=${getLeagueName()}&username=${getUsername(getAuthToken())";
+        request(
+            "GET", // Get request
+            querystring, 
+        ).then((response) => {
+            return response;
+        }).catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+    }
 
     //This useEffect hook is used to display the leagues the user is in.
     useEffect(() => {
@@ -57,6 +70,10 @@ const AuthContent = () => {
 
     const count = 1;
 
+    const handleEnterLeagueClick = (leagueName) => {
+        window.location.href = `/roster?league=${leagueName}`;
+    };
+
     const renderLeagues = () => {
         if (data.length === 0) {
             return <p>No leagues found.</p>;
@@ -65,7 +82,46 @@ const AuthContent = () => {
         return (
             <ul>
                 {data.map((league) => (
-                    <li key={league.id} className="card" style={{ width: "20rem", margin: "50px"}}>
+                    <div>
+                        <div>
+                            <div class="modal-button">
+                                <button type="button" class="Size" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    {league.leagueName}
+                                    <p></p>
+                                    {league.leagueName} 
+                                </button>
+                            </div>
+                        </div>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{league.leagueName}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Team Name: {getTeamName()}</p>
+                                        <p>League Ranking: </p>
+                                        <p>Join Code: {league.joinCode}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" className="btn btn-primary" onClick={() => handleEnterLeagueClick(league.leagueName)} style={{ margin: "5px" }} data-bs-dismiss="modal">Enter League</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        );
+    }
+
+    const enterLeague = () => {
+
+    }
+
+    /*
+    <li key={league.id} className="card" style={{ width: "20rem", margin: "50px"}}>
                         <h5 className="card-title">
                             {league.leagueName}
                         </h5>
@@ -86,11 +142,7 @@ const AuthContent = () => {
                             ))}
                         </div>
                     </li>
-                ))}
-            </ul>
-        );
-    }
-
+                    */
     // What shows up visually, based on the component's state.
     // "data" makes sure the data is not null/undefined. The rest maps over each element of data and prints it out in the <p> tag.
     // If nothing is in the component's current state, nothing renders.
