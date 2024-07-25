e3c8c31324f8269cc7d0aadecf9edc4d99d19b32
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-07-25 16:12:08 -0400

    adding and dropping players

--- src/components-pages/App.js
+++ src/components-pages/App.js
@@ -14,6 +14,7 @@ import Players from './Players';
 import League from './League';
 import DraftSettings from './DraftSettings';
 import DraftUI from './DraftUI';
+import { DropPlayer } from './AddAndDropPlayer';
 
 function App() {
   
